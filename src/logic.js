3b8b4de5cd19a257211986c1c3ec0c8c313f13d7
reveurguy <59417546+reveurguy@users.noreply.github.com>
2022-07-22 20:16:27 +0530

    find food

--- src/logic.js
+++ src/logic.js
@@ -94,7 +94,7 @@ function move(gameState) {
         });
     }
 
-    // possibleMoves = temp.map(move => move.dir)
+    possibleMoves = temp.map(move => move.dir)
     possibleMoves = Object.assign(...possibleMoves.map(k => ({ [k]: true })))
 
     // // Avoid hazards
